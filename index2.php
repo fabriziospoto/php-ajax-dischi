cf134c81189ab65ebdd8a4bd0770224e78771860
Fabrizio <fabriziospoto1@gmail.com>
2020-09-18 17:04:32 +0200

    Milestone 1
    Milestone 1

---
+++ index2.php
@@ -0,0 +1,34 @@
+<?php
+include 'db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;400;700&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="dist/app.css">
+        <title>php-ajax-dischi</title>
+    </head>
+    <body>
+        <header>
+
+        </header>
+        <main>
+            <div class="container">
+                <div class="box clearfix">
+                    <?php foreach ($database as $data) { ?>
+                        <div class="disc-box">
+                            <img src="<?php echo $data["poster"]; ?>" alt="poster-disco">
+                            <h4><?php echo $data["title"]; ?></h4>
+                            <h5><?php echo $data["author"]; ?></h5>
+                            <h6><?php echo $data["year"]; ?></h6>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </main>
+        <footer>
+        </footer>
+    </body>
+</html>
